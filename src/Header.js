218abbcb85a57cc90766f5942b25d53f5d02c30a
kodingpemula1 <andrebkng@gmail.com>
2023-06-09 10:17:57 +0800

    react-setup

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import foto from './logo-koding-akademi.jpg';
+import MyStyles from './normal.module.css';
+
+function Header() {
+
+}
+
+export default Header;
---
+++ src/MyButton.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class MyButton extends Component {
+    render() {
+        return <button className='button'>My Button</button>
+    }
+}
+
+export default MyButton;
